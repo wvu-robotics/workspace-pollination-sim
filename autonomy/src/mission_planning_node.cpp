ce80ee5b95381d1701d9a68ea78106580ab5ee70
Ibrahim <roughar@gmail.com>
2021-03-10 16:28:50 -0500

    Deleted Autonomy code from here, moved to separate repo

--- autonomy/src/mission_planning_node.cpp
+++
@@ -1,9 +0,0 @@
-#include <autonomy/mission_planning.hpp>
-
-int main(int argc, char** argv)
-{
-    ros::init(argc, argv,"mission_planning_node");
-    MissionPlanning missionPlanning;
-    missionPlanning.run();
-    return 0;
-}
